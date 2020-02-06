d76f3cf68c6673557c0c73d1a0c2ae6c66961261
Adiber <adiber.yt@gmail.com>
2020-02-06 17:48:19 +0100

    Minesweeper working properly
    Apparently Threads produce a lot of IndexOutOfBounds Exceptions with static HashMaps
    (made some compromise, but there are less effects now D: )

--- Minesweeper/app/src/main/java/at/htlkaindorf/minesweeper/bl/GameLogic.java
+++ Minesweeper/app/src/main/java/at/htlkaindorf/minesweeper/bl/GameLogic.java
@@ -14,6 +14,8 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import androidx.core.content.ContextCompat;
 import at.htlkaindorf.minesweeper.MyBounceInterpolator;
@@ -61,8 +63,11 @@ public class GameLogic {
             if(!field.containsKey(b.getId())) {
                 field.put(b.getId(), new Field(false, false, getBombsAround(b.getId()), b.getId()));
             }
-            //do after other if, so that no null pointer
-            if(b.getId() == first) {
+        }
+
+        //do after other, so that no null pointer and also no indexoutofboundy by recursiveNull()
+        for(Button b : buttons) {
+            if (b.getId() == first) {
                 //call this, to do the click
                 shortClick(b, buttons);
             }
@@ -112,7 +117,7 @@ public class GameLogic {
 
             return 1;
         } else if(field.get(bt.getId()).getMinesAround() == 0) { //clicked on zero
-                recursiveNull2(bt.getId(), buttons);
+                recursiveNull2(bt.getId(), buttons, 0);
             return 3;
         } else { //it is a "normal" button
             bt.setText("" + field.get(bt.getId()).getMinesAround());
@@ -164,6 +169,8 @@ public class GameLogic {
         return bombs;
     }
 
+
+    //DONT LOOK AT THIS (it does not work, because with threads there, for some reason, appear a lot of null pointer exceptions and index out of bounds)
     private static void recursiveNull(int id, Button[] buttons) {
 
         field.get(id).setDiscovered(true);
@@ -199,7 +206,7 @@ public class GameLogic {
                                         for(Button tb : buttons) {
                                             if(tb.getId() == tmp) {
                                                 tb.startAnimation(anim);
-                                                tb.setText("" + field.get(tmp).getMinesAround());
+                                                tb.setText("" + field.get(tmp).getMinesAround());   //here index out of bounds
                                                 tb.setTextColor((tb.getText() == "1" ? Color.BLUE : (tb.getText() == "2" ? Color.GREEN : Color.RED)));
                                                 field.get(tmp).setDiscovered(true);
                                             }
@@ -215,10 +222,7 @@ public class GameLogic {
 
     }
 
-    private static void recursiveNull2(int id, Button[] buttons) {
-
-        if(field.get(id).isDiscovered())
-            return;
+    private static void recursiveNull2(int id, Button[] buttons, int wave) {
 
         field.get(id).setDiscovered(true);
 
@@ -226,30 +230,36 @@ public class GameLogic {
             if(b.getId() == id) {
                 if(field.get(id).getMinesAround() == 0) {
                     b.setBackgroundTintList(ColorStateList.valueOf(Color.GRAY));
+                    final Animation anim = AnimationUtils.loadAnimation(b.getContext(), R.anim.bounce);
 
+                    MyBounceInterpolator interpolator = new MyBounceInterpolator(0.2, 20);
+                    anim.setInterpolator(interpolator);
+                    anim.setStartOffset(10*wave);
+                    b.startAnimation(anim);
                     for(int i = -1; i<=1; i++) {
                         for (int j = -1; j <= 1; j++) {
                             int tmp = id + (i * 10) + j;
-
-                            if (!field.containsKey(tmp)) {
-                                Log.d("Main", ""+tmp);
+                            if (!field.containsKey(tmp))
                                 continue;
-                            }
-                            if (!field.get(tmp).isMine() && !field.get(tmp).isFlagged()) {
-                                recursiveNull2(tmp, buttons);
+                            if(!field.get(tmp).isDiscovered() && !field.get(tmp).isMine() && !field.get(tmp).isFlagged()) {
+                                recursiveNull2(tmp, buttons, wave++);
                             }
                         }
                     }
-
-                } else {
-                    b.setText("" + field.get(id).getMinesAround());
-                    b.setTextColor((field.get(id).getMinesAround() == 1 ? Color.BLUE : (field.get(id).getMinesAround() == 2 ? Color.GREEN : Color.RED)));
                 }
+                else {
+                    b.setText("" + field.get(b.getId()).getMinesAround());
+                    b.setTextColor((field.get(b.getId()).getMinesAround()==1 ? Color.BLUE : (field.get(b.getId()).getMinesAround()==2?Color.GREEN : Color.RED)));
+                    field.get(b.getId()).setDiscovered(true);
+                    final Animation anim = AnimationUtils.loadAnimation(b.getContext(), R.anim.bounce);
 
-                break;
+                    MyBounceInterpolator interpolator = new MyBounceInterpolator(0.2, 20);
+                    anim.setInterpolator(interpolator);
+                    anim.setStartOffset(10*wave);
+                    b.startAnimation(anim);
+                }
             }
         }
-
 
     }
 
