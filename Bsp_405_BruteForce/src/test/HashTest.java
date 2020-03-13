fd9da8b8277e766a4b395d045f39208033e88b64
0Adiber <0Adiber@users.noreply.github.com>
2020-03-13 09:49:02 +0100

    changed maven to ant

---
+++ Bsp_405_BruteForce/src/test/HashTest.java
@@ -0,0 +1,24 @@
+package test;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.xml.bind.DatatypeConverter;
+
+
+public class HashTest {
+    
+    public static void main(String[] args) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            String password = "Lamar" + "Shington" + "9j0g3";
+            byte[] hash = md.digest(password.getBytes());
+            String hashAsHexString = DatatypeConverter.printHexBinary(hash);
+            System.out.println(hashAsHexString);
+        } catch (NoSuchAlgorithmException ex) {
+            Logger.getLogger(HashTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+}
