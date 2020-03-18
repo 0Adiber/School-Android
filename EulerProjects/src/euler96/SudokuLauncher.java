7d6518be4c6eb0eeeb90092c2f3a00592636912f
Adiber <adiber.yt@gmail.com>
2020-03-18 19:26:28 +0100

    Sudoku finished

--- EulerProjects/src/euler96/SudokuLauncher.java
+++ EulerProjects/src/euler96/SudokuLauncher.java
@@ -5,42 +5,73 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class SudokuLauncher {
     
-    public void performSolve() {
+    public void runSudokuCalculation() throws IOException, InterruptedException, ExecutionException {
+        ExecutorService pool = Executors.newFixedThreadPool(4);
+        List<Callable<Integer>> workers = new ArrayList<>();
         
+        String[] sudokus = loadData();
+        
+        for(String s : sudokus) {
+            workers.add(new SudokuWorker(s));
+        }
+
+//        workers.add(new SudokuWorker(sudokus[1]));
+        
+        List<Future<Integer>> res = pool.invokeAll(workers);
+        pool.shutdown();
+        
+        int sum = 0;
+        for(Future<Integer> f : res) {
+            int num = f.get();
+            sum+=num;
+        }
+        
+        System.out.println(sum);
     }
     
     public static void main(String[] args) {
+        try {
+            new SudokuLauncher().runSudokuCalculation();
+        } catch (IOException | InterruptedException | ExecutionException ex) {
+            Logger.getLogger(SudokuLauncher.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
         
+
     }
     
-    private Sudoku[] loadData() throws FileNotFoundException, IOException {
-        Sudoku[] sudokus = new Sudoku[50];
-        
-        String[] sudokuss = new String[50];
+    private String[] loadData() throws FileNotFoundException, IOException {
+        String[] sudokus = new String[50];
         
         String path = System.getProperty("user.dir") + File.separator + "src" + File.separator + "euler96" + File.separator + "sudoku.txt";
         FileReader fr = new FileReader(new File(path));
         BufferedReader br = new BufferedReader(fr);
         
-        String line = br.readLine();
-        int cc = 0, inner = 0;
-        while((line = br.readLine()) != null) {
+        String line;
+        int cc = -1, inner = 0;
+        String sudo = "";
+        while((line = br.readLine()) != null){
             if(line.startsWith("G")) {
                 cc++;
-                inner = 0;
+                sudokus[cc] = sudo;
+                sudo = "";
             } else {
-                if(sudokus[cc] == null)
-                    sudokus[cc] = new Sudoku();
-                int y = 0;
-                for(char c : line.toCharArray()) {
-                    sudokus[cc].setNum(inner, y++, Integer.parseInt(""+c));
-                }
-                inner++;
+                sudo+=line;
+                sudokus[cc] = sudo;
             }
-        }
+        };
         
         return sudokus;
     }
--- EulerProjects/src/euler96/SudokuWorker.java
+++ EulerProjects/src/euler96/SudokuWorker.java
@@ -5,6 +5,10 @@ import java.util.concurrent.Callable;
 public class SudokuWorker implements Callable<Integer>{
     
     String grid = "";
+    int arr[][] = new int[9][9];
+    
+    boolean finished = false;
+   
 
     public SudokuWorker(String grid) {
         this.grid = grid;
@@ -15,7 +19,131 @@ public class SudokuWorker implements Callable<Integer>{
     public Integer call() throws Exception {
         //returns top left three numbers
         
-        throw new Exception();
+        int cc = 0;
+        for(int i = 0; i<grid.length(); i++) {
+            arr[cc][i%9] = grid.toCharArray()[i]-'0';
+            if((i+1)%9 == 0)
+                cc++;
+        }
+        
+        bruting(0, 0);
+        
+        return Integer.parseInt(String.format("%d%d%d", arr[0][0], arr[0][1], arr[0][2]));
+    }
+    
+    public void bruting(int y, int x) {
+        if(x > 8) {
+            y++;
+            x=0;
+        }
+        
+        if(y > 8) {
+            finished = true;
+            return;
+        }
+        
+        if(arr[y][x] != 0) {
+            bruting(y, x+1);
+            
+        } else {
+            for(int num = 1; num<=9;num++) {
+                
+                if(isPossible(y, x, num)) {
+                    arr[y][x] = num;
+                    if(x == 8 && y == 8)
+                        finished = true;
+                    
+                    bruting(y, x+1);
+                    
+                    if(finished) return;
+                    arr[y][x] = 0;
+                    
+                } 
+                
+            }
+        }
+    }
+    
+    public boolean isPossible(int y, int x, int num) {
+        
+        //ROW
+        for (int val : arr[y]) {
+            if (val == num)
+                return false;
+        }
+        
+        //COLUMN
+        for (int yMov = 0; yMov < arr.length; yMov++) {
+            if (arr[yMov][x] == num)
+                return false;
+        }
+        
+        //BOX
+        int startY = Math.floorDiv(y, 3) * 3;
+        int startX = Math.floorDiv(x, 3) * 3;
+        for (int y1 = 0; y1 < 3; y1++) {
+            for (int x1 = 0; x1 < 3; x1++) {
+                if (arr[startY + y1][startX + x1] == num)
+                    return false;
+            }
+        }
+        return true;
+    }
+    
+    public boolean checkSudoku(){
+        //check row
+        for(int i = 0; i<9; i++) {
+            for(int j = 0; j<9; j++) {
+                for(int k = j+1; k<9; k++) {
+                    if(arr[i][j] == arr[i][k] && arr[i][j] != 0)
+                        return false;
+                }
+            }
+        }
+        
+        //check column
+        for(int i = 0; i<9; i++) {
+            for(int j = 0; j<9; j++) {
+                for(int k = j+1; k<9; k++) {
+                    if(arr[j][i] == arr[k][i] && arr[j][i] != 0)
+                        return false;
+                }
+            }
+        }
+        
+//        //check boxes
+        int brv = 0;
+        int bcv = 0;
+        
+        for(int x = 1; x<=9; x++) {
+            
+            for(int i = brv+0; i<3; i++) {
+                for(int j = bcv+0; j<3; j++) {
+                
+                    for(int k = brv+0; k<3; k++) {
+                        for(int l = bcv+0; l<3; l++) {
+                            
+                            if(i == k && j == l)
+                                continue;
+                            
+                            if(arr[i][j] == arr[k][l] && arr[i][j] != 0)
+                                return false;
+                            
+                        }
+                    }
+                    
+                }
+            }
+            bcv+=3;
+            
+            if(x%3==0) {
+                brv+=3;
+                bcv=0;
+            }
+                   
+        }
+        
+        return true;
     }
     
 }
